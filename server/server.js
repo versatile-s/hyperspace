24137ed1cd715c06e80c99432e5d0908c3d2512b
Daniel Metzger <kinaharo@gmail.com>
2016-09-23 16:02:22 -0700

    Create router Link router to server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 var express = require('express');
+var router = require('./router');
 
 var server = express();
 
 var port = process.env.port || 3000;
 
-server.get('/', function(req, res) {
-  res.send('You are now viewing / of hyperspace!');
-});
+server.use(router);
 
 server.listen(port, function() {
   console.log('Server is listening on port ' + port + '!');
