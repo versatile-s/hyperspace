bb1eebede68d3a65fe5649493425c17eeb50fb86
Daniel Metzger <kinaharo@gmail.com>
2016-09-23 21:57:44 -0700

    Add buttons that ping /login and /signup

--- client/app.js
+++ client/app.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-var ReactBox = React.createClass({
-  render: function(){
+export default class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      url: "http://localhost:3000"
+    };
+    this.signup = this.signup.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  signup (e) {
+    e.preventDefault();
+    fetch(this.state.url + '/signup', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  login (e) {
+    e.preventDefault();
+    fetch(this.state.url + '/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
+  render () {
     return (
-      <div className="box">
-        We're Live, Bitches.
+      <div>
+        <input type="button" value="signup" onClick={this.signup} />
+        <input type="button" value="login" onClick={this.login} />
       </div>
     );
   }
-});
+}
 
-ReactDOM.render(
-  <ReactBox />,
-  document.getElementById('content')
-);
+ReactDOM.render(<App />, document.getElementById('content'));
--- server/router.js
+++ server/router.js
@@ -11,96 +11,96 @@ router.route('/')
 router.route('/signup')
   .post(function (req, res) {
     console.log('Received POST at /signup');
-    res.send('Received POST at /signup');
+    res.send(JSON.stringify('Received POST at /signup'));
 });
 
 // existing user login
 router.route('/login')
   .post(function (req, res) {
     console.log('Received POST at /login');
-    res.send('Received POST at /login');
+    res.send(JSON.stringify('Received POST at /login'));
 });
 
 // view categories
 router.route('/categories')
   .get(function (req, res) {
     console.log('Received GET at /categories');
-    res.send('Received GET at /categories');
+    res.send(JSON.stringify('Received GET at /categories'));
 });
 
 // search categories
 router.route('/categories')
   .post(function (req, res) {
     console.log('Received POST at /categories');
-    res.send('Received POST at /categories');
+    res.send(JSON.stringify('Received POST at /categories'));
 });
 
 // view category
 router.route('/category')
   .get(function (req, res) {
     console.log('Received GET at /category');
-    res.send('Received GET at /category');
+    res.send(JSON.stringify('Received GET at /category'));
 });
 
 // add category
 router.route('/category')
   .post(function (req, res) {
     console.log('Received POST at /category');
-    res.send('Received POST at /category');
+    res.send(JSON.stringify('Received POST at /category'));
 });
 
 // remove link from category
 router.route('/category')
   .put(function (req, res) {
     console.log('Received PUT at /category');
-    res.send('Received PUT at /category');
+    res.send(JSON.stringify('Received PUT at /category'));
 });
 
 // add tag to category
 router.route('/addTag')
   .post(function (req, res) {
     console.log('Received POST at /addTag');
-    res.send('Received POST at /addTag');
+    res.send(JSON.stringify('Received POST at /addTag'));
 });
 
 // add link
 router.route('/link')
   .post(function (req, res) {
     console.log('Received POST at /link');
-    res.send('Received POST at /link');
+    res.send(JSON.stringify('Received POST at /link'));
 });
 
 // edit link
 router.route('/link')
   .put(function (req, res) {
     console.log('Received PUT at /link');
-    res.send('Received PUT at /link');
+    res.send(JSON.stringify('Received PUT at /link'));
 });
 
 // search links
 router.route('/searchLinks')
   .post(function (req, res) {
     console.log('Received POST at /searchLinks');
-    res.send('Received POST at /searchLinks');
+    res.send(JSON.stringify('Received POST at /searchLinks'));
 });
 
 // view preferences
 router.route('/preferences')
   .get(function (req, res) {
     console.log('Received GET at /preferences');
-    res.send('Received GET at /preferences');
+    res.send(JSON.stringify('Received GET at /preferences'));
 });
 
 // set preferences
 router.route('/preferences')
   .post(function (req, res) {
     console.log('Received POST at /preferences');
-    res.send('Received POST at /preferences');
+    res.send(JSON.stringify('Received POST at /preferences'));
 });
 
 // 404
 router.use('*', function(req, res) {
-  res.status(404).send('404, Sari Gurl');
+  res.status(404).send(JSON.stringify('404, Sari Gurl'));
 });
 
 module.exports = router;
