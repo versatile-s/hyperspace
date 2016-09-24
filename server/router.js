6c97067306d9d5f48f47016917d4a80bb962a0f1
Daniel Metzger <kinaharo@gmail.com>
2016-09-23 17:18:44 -0700

    Add bodyParser

--- server/router.js
+++ server/router.js
@@ -1,7 +1,6 @@
 var router = require('express').Router();
 
 router.use('*', function(req, res) {
-  console.log('Router is functioning.');
   res.send('You have reached hyperspace! We\'re not here right now, so please leave a message and we\'ll get back to you as soon as we can. Thanks, bye!');
 });
 
