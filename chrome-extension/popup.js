7d7ced6535cfb10c9fa2cebc987345c8d0ae6962
bryanmojica <bryan@trestian.com>
2016-09-26 15:17:09 -0700

    sending auth as stringified XML

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -8,63 +8,69 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
-var getCurrentTabUrl = function (callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
+
+window.onload = function() {
+
+  var getCurrentTabUrl = function (callback) {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+      // chrome.tabs.query invokes the callback with a list of tabs that match the
+      // query. When the popup is opened, there is certainly a window and at least
+      // one tab, so we can safely assume that |tabs| is a non-empty array.
+      // A window can only have one active tab at a time, so the array consists of
+      // exactly one tab.
+      var tab = tabs[0];
+
+      // A tab is a plain object that provides information about the tab.
+      // See https://developer.chrome.com/extensions/tabs#type-Tab
+      var url = tab.url;
+
+      // tab.url is only available if the "activeTab" permission is declared.
+      // If you want to see the URL of other tabs (e.g. after removing active:true
+      // from |queryInfo|), then the "tabs" permission is required to see their
+      // "url" properties.
+      console.assert(typeof url == 'string', 'tab.url should be a string');
+
+      console.log('CURRENT TABS URL IS', url);
+
+      // must used XMLHttpRequest in extension 
+      var xhr = new XMLHttpRequest();
+      var params = url;
+      xhr.open('POST', 'http://127.0.0.1:3000/link', true);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.send(params);
+    });
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+  getCurrentTabUrl();
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    console.log('CURRENT TABS URL IS', url);
-
-    // must used XMLHttpRequest in extension 
+  var authenticateUser = function () {
     var xhr = new XMLHttpRequest();
-    var params = url;
-    xhr.open('POST', 'http://127.0.0.1:3000/link', true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send(params);
-  });
-};
-
-getCurrentTabUrl();
-
-var authenticateUser = function () {
-  var xhr = new XMLHttpRequest();
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  var authenticated = false;
-  
-  xhr.open('POST', 'http://127.0.0.1:3000/login');
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xhr.send(JSON.stringify({username: username, password: password}), function(response) {
-    console.log('RESPONSE IS', response);
-  });
-
-
-  console.log('username and pass are', username + password);
-
-  if (authenticated) {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var authenticated = false;
     
-  }
-};
+    xhr.open('POST', 'http://127.0.0.1:3000/login');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(JSON.stringify({username: username, password: password}), function(response) {
+      console.log('RESPONSE IS', response);
+    });
 
-setInterval(authenticateUser, 2000);
+    console.log('username and pass are', username + password);
+
+    if (authenticated) {
+    }
+  };
+
+  document.getElementById('submit').onclick = function(e) {
+    e.preventDefault();
+    authenticateUser();
+  };
+
+};
