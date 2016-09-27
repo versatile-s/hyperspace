99c6c85dca13fe1423ed75389a56157a0e981566
bryanmojica <bryan@trestian.com>
2016-09-27 15:14:24 -0700

    create userSignIn component

--- chromeApp/chrome-extension/main.js
+++ chromeApp/chrome-extension/main.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import UserSignIn from './userSignIn.js';
 
 
 class ChromeApp extends Component {
   constructor (props) {
     super(props);
+  
+  this.state = {
+    authenticated: false;
+  };
+
   }
+
+
 
   render () {
     return (
       <div>
         <h1>WE R NOW RENDERING REACT IN R EXTENSION :) </h1>
+        <UserSignIn />
+        <Worker />
       </div>
     );
   }
---
+++ chromeApp/chrome-extension/userSignIn.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+
+
+class UserSignIn extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div className="authBody">
+        <form>
+          <input id="username"/>
+          <input id="password"/>
+          <button className="submit">log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserSignIn;
+
+
+
+
+
+
+      
+        <div class="authBody" style="margin-top:30px; text-align:center;">
+        <div id="text" style="font-weight: 100 !Important; font-family: Roboto; text-align: center;">Welcome to hyperspace. Please login.</div>
+      <form class="userLogin" style="margin-top:30px;">
+        <input id="username" value="" placeholder="your username" type="text" style="line-height: 24px;
+    font-size: 16px;
+    width: 240px;
+    text-align: center;
+    font-family: Roboto;
+    font-weight: 400;
+    border-radius: 32px;
+    border: none; height: 40px;">
+        <input id="password" value="" placeholder="password" type="password" style="line-height: 24px;
+    font-size: 16px;
+    width: 240px;
+    text-align: center;
+    font-family: Roboto;
+    font-weight: 400;
+    border-radius: 32px;
+    border: none; height: 40px; margin-top: 15px;">
+        <div class="submit" style="text-align:right;">
+          <button class="submit" id="submit" style="border-radius: 0px !important;
+    background: none;
+    border: 1px solid #000;
+    padding: 7px 20px;
+    margin-right: 40px;
+    margin-top: 20px; margin-bottom: 20px;">log in :)</button>
+        </div>
+        </div>
