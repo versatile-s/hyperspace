f71a0ccd9dc706d346303a4226bf186a3b2cb754
Daniel Metzger <kinaharo@gmail.com>
2016-09-26 21:29:49 -0700

    Build fetch call to /category in side.js

--- client/side.js
+++ client/side.js
@@ -13,9 +13,33 @@ class Side extends Component {
 
   handleClick (e) {
     e.preventDefault();
-    var categoryTitle = e.target.value;
+    var categoryTitle = e.target.innerHTML;
     console.log(categoryTitle);
     // send user to their category of categoryTitle
+    fetch('http://localhost:3000' + '/category', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        categoryTitle: categoryTitle
+      })
+    }).then((response) => {
+      response.text().then((res) => {
+        console.log(res);
+      }); 
+    })
+    .catch((error) => {
+      error.text().then((err) => {
+        console.log(err);
+      }); 
+    });
+  }
+
+  focused (e) {
+    e.target.value = '';
   }
 
   render () {
@@ -25,10 +49,10 @@ class Side extends Component {
           Options
           <div className="side-menu-search container">
             <div className="side-menu-search">
-              Category:<input className="side-menu-text-box" type="text" defaultValue="enter Category here" />
+              Category:<input className="side-menu-text-box" type="text" defaultValue="enter Category here" onFocus={this.focused} />
             </div>
             <div className="side-menu-search">
-              Tag:<input className="side-menu-text-box" type="text" defaultValue="enter Category here" />
+              Tag:<input className="side-menu-text-box" type="text" defaultValue="enter Tag here" onFocus={this.focused}/>
             </div>
             <Link className="side-menu-link" to='/category'><button>Link'd to /category</button></Link>
           </div>
@@ -40,7 +64,7 @@ class Side extends Component {
               {this.state.categories.map((category) => {
                 return (
                   <tr>
-                    <td className="side-menu-category" onClick={this.handleClick}>{category}</td>
+                    <td className="side-menu-category" onClick={this.handleClick} >{category}</td>
                   </tr>
                 );
               })}
