49c03820cd616f5765bf9d925ea95caf8c301c31
Aljosha Novakovic <ollynov@gmail.com>
2016-09-28 17:18:55 -0700

    Express Session dependency and bare-bones set up

--- server/server.js
+++ server/server.js
@@ -3,18 +3,22 @@ var router = require('./router');
 var bodyParser = require('body-parser');
 var path = require('path');
 var history = require('connect-history-api-fallback');
+var session = require('express-session');
 
 
 var server = express();
 
 server.use(history());
-
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ 'extended': false }));
-
 server.use(express.static(path.join(__dirname, '../client')));
-
 server.use('/', router);
+
+app.use(session({
+  secret: 'Our Secret',
+  resave: false,
+  saveUninitialized: true
+}));
 
 var port = process.env.port || 3000;
 
