3afc1fd26c2da468fe12abdb6e395377141f3402
Aljosha Novakovic <ollynov@gmail.com>
2016-10-03 20:09:37 -0700

    fixing rebase

--- server/router.js
+++ server/router.js
@@ -2,55 +2,42 @@
 var utils = require('./utilities');
 var path = require('path');
 
-         // HOMEPAGE //
-// router.route('/')
-//   .get(function (req, res) {
-//     console.log('Received GET at /');
-//     // if (utils.isAuth(req, res)) {
-//     //   res.redirect('/dashboard');
-//     //   console.log('redirect to dashboard');
-//     // }
-//     // The above is not doing anything at the moment because we are using routers
-//     res.send('Received GET at /');
-//   });
 
 /*********************************/
 /*********************************/
            //USERS//
 /*********************************/
 /*********************************/
-// new user signup
-<<<<<<< f4ee7ce347459cb7cb8074d6b338a6d8fb6d7c11
-=======
-router.route('/signup')
-  .post(function (req, res) {
-    console.log('Received POST at /signup');
-    // if (utils.isAuth(req, res)) {
-    //   res.redirect('/dashboard');
-    //   console.log('redirect to dashboard');
-    // }
-    utils.createUser(req.body.username, req.body.password);
-    res.send('User created');
-  });
+module.exports = (router) => {
+  router.route('/signup')
+    .post(function (req, res) {
+      console.log('Received POST at /signup');
+      // if (utils.isAuth(req, res)) {
+      //   res.redirect('/dashboard');
+      //   console.log('redirect to dashboard');
+      // }
+      utils.createUser(req.body.username, req.body.password);
+      res.send('User created');
+    });
 
-// existing user login
-router.route('/login')
-  .post(function (req, res) {
-    utils.loginUser(req, res);
-  });
+  // existing user login
+  router.route('/login')
+    .post(function (req, res) {
+      utils.loginUser(req, res);
+    });
 
-router.route('/logout')
-  .get(function (req, res) {
-    console.log('Received GET at /logout');
-    utils.logoutUser(req, res);
-  });
+  router.route('/logout')
+    .get(function (req, res) {
+      console.log('Received GET at /logout');
+      utils.logoutUser(req, res);
+    });
 
-router.route('/users')
-  .put(function (req, res) {
-    console.log('Received PUT at /users');
-    utils.updateUser(req, res);
-    console.log('User updated');
-  });
+  router.route('/users')
+    .put(function (req, res) {
+      console.log('Received PUT at /users');
+      utils.updateUser(req, res);
+      console.log('User updated');
+    });
 
 /*********************************/
 /*********************************/
@@ -58,30 +45,30 @@ router.route('/users')
 /*********************************/
 /*********************************/
 
-router.route('/categoryData')
-  .post(function (req, res) {
-    console.log('Searching for categoryData');
-    utils.getCategoryData(req, res);
-  });
+  router.route('/categoryData')
+    .post(function (req, res) {
+      console.log('Searching for categoryData');
+      utils.getCategoryData(req, res);
+    });
 
-// view category
-router.route('/category')
-  .get(function (req, res) {
-    console.log('Received GET at /category');
-    res.send('Received GET at /category');
-  })
-// add category
-  .post(function (req, res) {
-    console.log('Received POST at /category');
-    utils.saveCategoryPage(req, res);
-    res.send('Received POST at /category');
-  })
-// update a category
-  .put(function (req, res) {
-    console.log('Received PUT at /category');
-    utils.updateCategoryPage(req, res);
-    res.send('Received PUT at /category');
-  });
+  // view category
+  router.route('/category')
+    .get(function (req, res) {
+      console.log('Received GET at /category');
+      res.send('Received GET at /category');
+    })
+  // add category
+    .post(function (req, res) {
+      console.log('Received POST at /category');
+      utils.saveCategoryPage(req, res);
+      res.send('Received POST at /category');
+    })
+  // update a category
+    .put(function (req, res) {
+      console.log('Received PUT at /category');
+      utils.updateCategoryPage(req, res);
+      res.send('Received PUT at /category');
+    });
 
 
   /*********************************/
@@ -164,5 +151,8 @@ router.route('/category')
     res.sendFile(path.join(__dirname, '../client/index.html' ));
   });
   return router;
-};
+
+
+}
+
 
